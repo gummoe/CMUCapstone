0d9f15b18d06d37cb4c2e93406d4add627e4f322
cassie9082 <caoyl767@hotmail.com>
2014-03-26 16:36:47 -0400

    cluster and TSP
    cluster and TSP

---
+++ greedyTSP.py
@@ -0,0 +1,34 @@
+import random
+
+matrix = [[0, 1.7, 1.7, 1.7, 2.0],[2.1, 0, 1.7, 1.8, 1.3],[2.6, 2.2, 0, 2.0, 1.7],[1.2, 1.5, 2.1, 0, 2.4],[1.9, 2.1, 1.8, 2.3, 0]]
+
+tour = [0]
+rowNum = 0
+while(len(tour) < len(matrix)):
+	min_val = 10000
+	for col in range(0, len(matrix)):
+		if matrix[rowNum][col] == 0:
+			continue
+		elif (col in tour) == True:
+			continue
+		else:
+			min_val = min(min_val, matrix[rowNum][col])
+
+	temp = []
+	for i in range(0, len(matrix)):
+		if matrix[rowNum][i] == min_val:
+			if (i in tour) == False:
+				temp.append(i)
+	rowNum = random.choice(temp)
+	tour.append(rowNum)
+
+tour.append(0)
+for num in range(0, len(tour)):
+    tour[num] = tour[num]+1
+
+
+
+print(tour)
+
+   
+
